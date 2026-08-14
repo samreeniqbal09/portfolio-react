40eb9751f01ff13dafa7443a067eac593aaf91c2
Samreen Iqbal <samreeniqbal0987@gmail.com>
2026-08-14 16:04:02 +0500

    Add scroll animations, SEO meta tags, and Open Graph image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,24 @@ import Hero from '../components/Hero.jsx'
 import About from '../components/About.jsx'
 import Skills from '../components/Skills.jsx'
 import Contact from '../components/Contact.jsx'
+import FadeInSection from '../components/FadeInSection.jsx'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <About />
-      <Skills />
-      <Contact />
+
+      <FadeInSection>
+        <About />
+      </FadeInSection>
+
+      <FadeInSection delay={0.1}>
+        <Skills />
+      </FadeInSection>
+
+      <FadeInSection delay={0.1}>
+        <Contact />
+      </FadeInSection>
     </>
   )
 }
